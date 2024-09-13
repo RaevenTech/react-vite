379e0af5a9f833f04c4f876a96af4df160c9c18d
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2024-09-13 16:16:19 +0100

    else if

--- bro-code/src/BtnClick.jsx
+++ bro-code/src/BtnClick.jsx
@@ -2,12 +2,23 @@ import "./btnClick.css"
 
 const BtnClick = () => {
 
-  const handleClick = () => {
-    console.log("OUCH!")
+  let count = 0
+
+  const handleClick = (name) => { 
+    if(count < 3){
+      count++
+      console.log(`${name} you clicked ${count} times`)
+    }else if(count === 3){
+      console.log(`${name} stop clicking me`)
+    }else{
+      console.log(`OK ${name} that's enough!!!`)
+    }
   }
+ 
+  
 
   return (
-    <button className="btn" onClick={handleClick}>Click me</button>
+    <button className="btn" onClick={() => handleClick("Bro")}>Click me</button>
   )
 }
 
